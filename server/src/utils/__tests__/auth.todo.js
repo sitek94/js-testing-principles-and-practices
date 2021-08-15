34dee220fc29c67761c1ed48b243409a7389326c
Maciek <msitkowski94@gmail.com>
2021-08-15 11:24:41 +0200

    feat(unit-tests): add 'userToJSON' unit test

--- server/src/utils/__tests__/auth.todo.js
+++ server/src/utils/__tests__/auth.todo.js
@@ -1,4 +1,4 @@
-const {isPasswordAllowed} = require('../auth')
+const {isPasswordAllowed, userToJSON} = require('../auth')
 
 test('isPasswordAllowed only allows some passwords', () => {
   // Empty string
@@ -18,25 +18,20 @@ test('isPasswordAllowed only allows some passwords', () => {
 })
 
 test('userToJSON excludes secure properties', () => {
-  // Here you'll need to create a test user object
-  // pass that to the userToJSON function
-  // and then assert that the test user object
-  // doesn't have any of the properties it's not
-  // supposed to.
-  // Here's an example of a user object:
-  // const user = {
-  //   id: 'some-id',
-  //   username: 'sarah',
-  //   // ↑ above are properties which should
-  //   // be present in the returned object
-  //
-  //   // ↓ below are properties which shouldn't
-  //   // be present in the returned object
-  //   exp: new Date(),
-  //   iat: new Date(),
-  //   hash: 'some really long string',
-  //   salt: 'some shorter string',
-  // }
+  const user = {
+    id: 'some-id',
+    username: 'sarah',
+    exp: new Date(),
+    iat: new Date(),
+    hash: 'some really long string',
+    salt: 'some shorter string',
+  }
+  const jsonUser = userToJSON(user)
+
+  expect(jsonUser).toEqual({
+    id: 'some-id',
+    username: 'sarah',
+  })
 })
 
 //////// Elaboration & Feedback /////////
