df30734c6d6618dc7f9730aace89272f53238414
Maciek <msitkowski94@gmail.com>
2021-08-24 07:17:35 +0200

    feat(whats-a-mock): finish 1st exercise

--- other/whats-a-mock/__tests__/thumb-war.1.todo.js
+++ other/whats-a-mock/__tests__/thumb-war.1.todo.js
@@ -1,19 +1,23 @@
 // monkey-patching
 import thumbWar from '../thumb-war'
 // import the utils module (see hint #1 at the bottom of the file)
+import * as utils from '../utils'
 
 test('returns winner', () => {
   // keep track of the original `getWinner` utility function (see hint #2)
+  const originalGetWinner = utils.getWinner
   // overwrite the utils.getWinner function with
   // our own that always returns the second player (see hint #3)
+  utils.getWinner = (p1, p2) => p2
 
   const winner = thumbWar('Ken Wheeler', 'Kent C. Dodds')
   // change this assertion to be more for a specific player
   // (like 'Kent C. Dodds', see hint #4):
-  expect(['Ken Wheeler', 'Kent C. Dodds'].includes(winner)).toBe(true)
+  expect(winner).toBe('Kent C. Dodds')
 
   // restore the originalGetWinner function so other tests don't break
   // (see hint #5)
+  utils.getWinner = originalGetWinner
 })
 
 /*
